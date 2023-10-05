0569f1b61dad5d5a5c10e7b5a705559e96ebc4fe
Kathryn Huxtable <kathryn@kathrynhuxtable.org>
2023-10-05 07:35:16 -0500

    Added ngrx dependencies

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -16,7 +16,7 @@
 
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -43,6 +43,9 @@ import { RatingModule } from 'primeng/rating';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { TableModule } from 'primeng/table';
 import { ToolbarModule } from 'primeng/toolbar';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
 	declarations: [
@@ -75,6 +78,9 @@ import { ToolbarModule } from 'primeng/toolbar';
 		SelectButtonModule,
 		AvatarModule,
 		OverlayPanelModule,
+		StoreModule.forRoot({}, {}),
+		EffectsModule.forRoot([]),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
 	],
 	providers: [
 		[
